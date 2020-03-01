23a2a47002e2de51d0166f1f61337157ec99f624
fwcd <fwcdmail@gmail.com>
2020-03-01 13:23:15 +0100

    Provide default as: parameter for ParserCombinators DSL

--- Sources/ParserCombinators/ParserCombinators.swift
+++ Sources/ParserCombinators/ParserCombinators.swift
@@ -1,10 +1,10 @@
 /// A parser for an alternative.
-public func alt<L, R, A>(_ left: L, _ right: R, as: A.Type) -> AltParser<L, R, A> where L: Parser, R: Parser, A: Alt, A.Left == L.Value, A.Right == R.Value {
+public func alt<L, R, A>(_ left: L, _ right: R, as: A.Type = A.self) -> AltParser<L, R, A> where L: Parser, R: Parser, A: Alt, A.Left == L.Value, A.Right == R.Value {
     return AltParser(left: left, right: right)
 }
 
 /// A parser for a sequence/concatenation.
-public func seq<L, R, S>(_ left: L, _ right: R, as: S.Type) -> SeqParser<L, R, S> where L: Parser, R: Parser, S: Seq, S.Left == L.Value, S.Right == R.Value {
+public func seq<L, R, S>(_ left: L, _ right: R, as: S.Type = S.self) -> SeqParser<L, R, S> where L: Parser, R: Parser, S: Seq, S.Left == L.Value, S.Right == R.Value {
     return SeqParser(left: left, right: right)
 }
 
