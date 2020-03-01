6253661b9a784b624ddde6b029abd0b0e39812d3
fwcd <fwcdmail@gmail.com>
2020-03-01 13:33:39 +0100

    Make SimpleAlt and SimpleSeq the default types in the DSL

--- Sources/ParserCombinators/ParserCombinators.swift
+++ Sources/ParserCombinators/ParserCombinators.swift
@@ -1,10 +1,20 @@
-/// A parser for an alternative.
-public func alt<L, R, A>(_ left: L, _ right: R, as: A.Type = A.self) -> AltParser<L, R, A> where L: Parser, R: Parser, A: Alt, A.Left == L.Value, A.Right == R.Value {
+/// A parser for an alternative with a custom type.
+public func alt<L, R, A>(_ left: L, _ right: R, as: A.Type) -> AltParser<L, R, A> where L: Parser, R: Parser, A: Alt, A.Left == L.Value, A.Right == R.Value {
     return AltParser(left: left, right: right)
 }
 
+/// A parser for an alternative.
+public func alt<L, R>(_ left: L, _ right: R) -> AltParser<L, R, SimpleAlt<L.Value, R.Value>> where L: Parser, R: Parser {
+    return AltParser(left: left, right: right)
+}
+
+/// A parser for a sequence/concatenation with a custom type.
+public func seq<L, R, S>(_ left: L, _ right: R, as: S.Type) -> SeqParser<L, R, S> where L: Parser, R: Parser, S: Seq, S.Left == L.Value, S.Right == R.Value {
+    return SeqParser(left: left, right: right)
+}
+
 /// A parser for a sequence/concatenation.
-public func seq<L, R, S>(_ left: L, _ right: R, as: S.Type = S.self) -> SeqParser<L, R, S> where L: Parser, R: Parser, S: Seq, S.Left == L.Value, S.Right == R.Value {
+public func seq<L, R>(_ left: L, _ right: R) -> SeqParser<L, R, SimpleSeq<L.Value, R.Value>> where L: Parser, R: Parser {
     return SeqParser(left: left, right: right)
 }
 
--- Tests/ParserCombinatorsTests/ParserCombinatorsTests.swift
+++ Tests/ParserCombinatorsTests/ParserCombinatorsTests.swift
@@ -15,7 +15,7 @@ final class ParserCombinatorsTests: XCTestCase {
     }
 
     private func testAlt() {
-        let alternative = alt(const("L"), alt(const("R1"), const("R2"), as: SimpleAlt.self), as: SimpleAlt.self)
+        let alternative = alt(const("L"), alt(const("R1"), const("R2")))
         XCTAssertEqual(alternative.parseValue(from: "L"), SimpleAlt.left("L"))
         XCTAssertEqual(alternative.parseValue(from: "R12"), SimpleAlt.right(SimpleAlt.left("R1")))
         XCTAssertNil(alternative.parseValue(from: "R3"))
