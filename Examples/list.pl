0ca7faa843aebe101040f56d05dcaa613510d177
fwcd <fwcdmail@gmail.com>
2020-03-19 18:45:29 +0100

    Fix concat predicate

--- Examples/list.pl
+++ Examples/list.pl
@@ -2,4 +2,4 @@ list(empty).
 list(cons(E, L)) :- list(L).
 
 concat(empty, R, R).
-concat(cons(E, L), R, LR) :- concat(L, R, LR), cons(E, LR).
+concat(cons(E, L), R, cons(E, LR)) :- concat(L, R, LR).
