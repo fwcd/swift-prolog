edc7aa228ed342c28f58e4036639562ac6c801f0
fwcd <fwcdmail@gmail.com>
2020-09-29 22:03:47 +0200

    Update package name to match repo name

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "Prolog",
+    name: "swift-prolog",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .executable(
